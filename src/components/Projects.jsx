e1e3bc413b01f6e65553f76a9c9db85067b8aafd
Alexandra Campo <alexandracampo86@gmail.com>
2023-04-28 12:29:12 +0200

    finish project section features

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,8 @@ import proyecto2 from '../images/proyecto2.JPG';
 import proyecto3 from '../images/proyecto3.JPG';
 import proyecto4 from '../images/proyecto4.JPG';
 import proyecto5 from '../images/proyecto5.JPG';
-import iconWeb from '../images/icon-web.png';
-import iconGit from '../images/icon-github.png'
+import iconWeb from '../images/icon-web1.png';
+import iconGit from '../images/icon-git.png'
 
 
 
@@ -17,40 +17,96 @@ function Projects(props) {
                 {"/Proyectos"}
             </h1>
             <ul className='projects__list' >
-                <li>
+
+                <li className={`projects-li ${props.theme}`}>
+                    <p className='title-project'>MY FIRST LAYOUT</p>
                     <img className="card-img" src={proyecto1} alt="captura pantalla de la web"></img>
-                    <div className={`card-info ${props.theme}`}>
-                        <a className='links-projects' href=""><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
-                        <a className='links-projects' href=""><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
+
+                    <div className='project-techologies'>
+                        <p className='tool'>HTML5</p>
+                        <p className='tool' >CSS3</p>
+                        <p className='tool'>Sass</p>
                     </div>
 
+                    <div className='card-links'>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://alexandracampo.github.io/my-first-layout-web/"><img src={iconWeb} className="icon-web" alt="enlace la web" />
+                        </a>
+
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://github.com/alexandracampo/my-first-layout-web"><img src={iconGit} className="icon-git" alt="enlace a git" />
+                        </a>
+                    </div>
+
+
+
                 </li>
-                <li>
+                <li className={`projects-li ${props.theme}`}>
+                    <p className='title-project'>TARJETAS DE VISITA</p>
                     <img className="card-img" src={proyecto2} alt="captura pantalla de la web"></img>
-                    <div className={`card-info ${props.theme}`}>
-                        <a className='links-projects' href=""><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
-                        <a className='links-projects' href=""><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
+
+                    <div className='project-techologies'>
+                        <p className='tool'>HTML5</p>
+                        <p className='tool' >CSS3</p>
+                        <p className='tool'>Sass</p>
+                        <p className='tool'>Javascript</p>
+                    </div>
+
+                    <div className='card-links'>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://alexandracampo.github.io/web-tarjetas-de-visita/"><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://github.com/alexandracampo/web-tarjetas-de-visita"><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
                     </div>
                 </li>
-                <li>
+
+                <li className={`projects-li ${props.theme}`}>
+                    <p className='title-project'>WEB DE PROYECTOS</p>
                     <img className="card-img" src={proyecto3} alt="captura pantalla de la web"></img>
-                    <div className={`card-info ${props.theme}`}>
-                        <a className='links-projects' href=""><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
-                        <a className='links-projects' href=""><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
+
+                    <div className='project-techologies'>
+                        <p className='tool'>HTML5</p>
+                        <p className='tool' >CSS3</p>
+                        <p className='tool'>Sass</p>
+                        <p className='tool'>Javascript</p>
+                        <p className='tool'>React</p>
+
+                    </div>
+
+                    <div className='card-links'>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://alexandracampo.github.io/web-de-proyectos/"><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://github.com/alexandracampo/web-de-proyectos"><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
                     </div>
                 </li>
-                <li>
+
+                <li className={`projects-li ${props.theme}`}>
+                    <p className='title-project'>API HOGWARTS</p>
                     <img className="card-img" src={proyecto4} alt="captura pantalla de la web"></img>
-                    <div className={`card-info ${props.theme}`}>
-                        <a className='links-projects' href=""><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
-                        <a className='links-projects' href=""><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
+
+                    <div className='project-techologies'>
+                        <p className='tool'>HTML5</p>
+                        <p className='tool' >CSS3</p>
+                        <p className='tool'>Sass</p>
+                        <p className='tool'>Javascript</p>
+                        <p className='tool'>React</p>
+                    </div>
+
+                    <div className='card-links' >
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://alexandracampo.github.io/harry-potter-react/"><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://github.com/alexandracampo/harry-potter-react"><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
                     </div>
                 </li>
-                <li>
+
+                <li className={`projects-li ${props.theme}`}>
+                    <p className='title-project'>DRINKING API COCKTAILS</p>
                     <img className="card-img" src={proyecto5} alt="captura pantalla de la web"></img>
-                    <div className={`card-info ${props.theme}`}>
-                        <a className='links-projects' href=""><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
-                        <a className='links-projects' href=""><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
+
+                    <div className='project-techologies'>
+                        <p className='tool'>HTML5</p>
+                        <p className='tool' >CSS3</p>
+                        <p className='tool'>Sass</p>
+                        <p className='tool'>Javascript</p>
+                    </div>
+
+                    <div className='card-links'>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://alexandracampo.github.io/buscador-cocteles/"><img src={iconWeb} className="icon-web" alt="enlace la web" /></a>
+                        <a className='links-projects' target="_blank" rel="noopener noreferrer" href="https://github.com/alexandracampo/buscador-cocteles"><img src={iconGit} className="icon-git" alt="enlace a git" /></a>
                     </div>
                 </li>
             </ul>
