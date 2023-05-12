3baa7990b697d98aaaaee28da46dbdb8a3686402
Alexandra Campo <alexandracampo86@gmail.com>
2023-05-12 12:58:06 +0200

    change text introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -10,13 +10,16 @@ function Introduction(props) {
 
             <div className="text">
                 <p className="text__box1">
-                    Hi! Me llamo Alexandra, vivo en Mallorca y me acabo de graduar en un increíble bootcamp de desarrollo web. Mi trayectoria profesional ha transcurrido principalmente en el ámbito administrativo. Me gradué en Administración de Empresas y fué ahí, trabajando en mi TFG, donde por fin decidí que tenía que desviar mi camino hacia el mundo de la programación.
+                    Hi there! ⚡
+                    <br />
+                    <br />
+                    Soy Alexandra y si has llegado hasta aquí es porque te interesa la programación o porque te he enviado el link a traición para que me aumente el número de visitas de la web.  Sea como sea, te quiero contar un par de cosas sobre mí:
                 </p>
                 <p className="text__box2">
-                    Me encanta programar y maquetar, y más allá de la parte técnica, soy una persona comunicativa, responsable, organizada y totalmente adaptable a los cambios. Todos mis proyectos de equipo se han desarrollado bajo una filosofía Agile, metodología Scrum y en remoto, hemos sido capaces de coordinarnos, organizarnos y practicar entre nosotros una comunicación efectiva donde el feedback era parte importante del trabajo.
+                    Soy inquieta por naturaleza, me gusta el movimiento, tanto físico como mental, lo cual se refleja en que me encanta viajar y me encanta programar. Lo de viajar lo descubrí hace muchos años y en todo este tiempo he viajado a lugares como Japón, California, China y muchos países de Europa. Sin embargo, mi pasión por la programación la descubrí hace menos tiempo, pero a día de hoy puedo decir que tengo una base sólida que me permite utilizar Javascript, HTML, CSS, o React, entre otras herramientas.
                 </p>
                 <p className="text__box3">
-                    Te invito a navegar por mi web, a seguirme en mis redes sociales y a que contactes conmigo para cualquier duda o sugerencia. Welcome!
+                    Creo que la mejor forma de que veas lo que soy capaz de hacer es visitando mis proyectos, por eso más abajo tienes acceso a ellos, tanto al repositorio de Github como a la propia web del proyecto. Te animo a que los visites y a que contactes conmigo para cualquier duda o sugerencia que tengas sobre ellos o sobre cualquier cosa. También puedes seguirme en mis redes sociales, en el apartado 'Contacto' tienes los enlaces. ¡Bienvenid@!
                 </p>
             </div>
         </div>
